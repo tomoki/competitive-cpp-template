719cfd0ddbb16902a8f353cb33365b7228633d59
Tomoki Imai <tomo832@gmail.com>
2022-11-01 23:40:04 +0900

    Add mac configuration

---
+++ copy.sh
@@ -0,0 +1,7 @@
+cp -r $(dirname "$0")/.vscode       $1
+cp -r $(dirname "$0")/test          $1
+cp -r $(dirname "$0")/main.cpp      $1
+cp -r $(dirname "$0")/.clang-format $1
+cp -r $(dirname "$0")/.editorconfig $1
+cp -r $(dirname "$0")/.gitignore    $1
+cp -r $(dirname "$0")/CMakeLists.txt $1

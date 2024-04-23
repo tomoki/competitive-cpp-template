cfd3f7bd5884b3c7adaa2299d2c78c3d0385e0f6
Tomoki Imai <tomo832@gmail.com>
2024-04-23 23:53:58 +0900

    Remove vs files

--- vs/unit-test/main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-int execute_main_with_dummy_stds(const std::string& stdin_file_name, const std::string& expected_file_name, const std::string& stdout_file_name, const std::string& stderr_file_name);

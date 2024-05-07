ceeb69c75b9989d5e4b31ab0a5945735f90e914e
Tomoki Imai <tomo832@gmail.com>
2024-05-08 02:32:35 +0900

    Fix mac build

--- main.cpp
+++ main.cpp
@@ -227,7 +227,9 @@ void output_with_header(const std::string& filename, const std::string& header) 
     std::cout << "-----------------" << std::endl;
 }
 
+#ifdef _WIN32
 __declspec(dllexport)
+#endif
 int execute_main_with_dummy_stds(const std::string& stdin_file_name, const std::string& expected_file_name, const std::string& stdout_file_name, const std::string& stderr_file_name)
 {
 

17a95fa5709e14edc9a9d5437573c96d7b23b9a0
Tomoki Imai <tomo832@gmail.com>
2024-05-21 03:16:57 +0900

    Do not copy stdout/stderr files

--- copy.sh
+++ copy.sh
@@ -1,6 +1,8 @@
 mkdir -p $1
 cp -r $(dirname "$0")/.vscode       $1
 cp -r $(dirname "$0")/test          $1
+rm -r $1/test/*.stderr
+rm -r $1/test/*.stdout
 cp -r $(dirname "$0")/main.cpp      $1
 cp -r $(dirname "$0")/.clang-format $1
 cp -r $(dirname "$0")/.editorconfig $1
